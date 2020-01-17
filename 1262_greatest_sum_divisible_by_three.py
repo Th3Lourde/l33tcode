ff6b976b0ed7c615a90e9ee6e4e3396ca3caba3a
Eli B Sylvia-Lourde <ebs2@SAH-LIB2-M01.local>
2020-01-17 15:20:52 -0800

    progress

--- 1262_greatest_sum_divisible_by_three.py
+++ 1262_greatest_sum_divisible_by_three.py
@@ -1,13 +1,25 @@
 
+'''
+Still working on problem
+'''
 
 class Solution:
     def maxSumDivThree(self, nums):
         def getTarget(currentElement):
             tmp =  currentElement//3
             if tmp*3 < currentElement:
-                return currentElement - (tmp+1)*3
+                return (tmp+1)*3 - currentElement
             elif tmp*3 > currentElement:
-                return currentElement - tmp*3
+                return tmp*3 - currentElement
+
+
+        def constructTarget(target, keys, d):
+            '''
+            So keys are already sorted in descending order
+            search for the largest element < target
+
+
+            '''
 
 
 
@@ -29,6 +41,7 @@ class Solution:
 
         i = 0
         while True:
+            newSum = []
 
             if i > len(keys)-1:
                 break
@@ -36,14 +49,29 @@ class Solution:
             currentElement = keys[i]
 
             if currentElement%3 == 0:
-                ans.append(currentElement)
+                # ans.append(currentElement)
+                newSum.append(currentElement)
                 print("{} is divisible by three".format(currentElement))
 
             elif currentElement%3 != 0:
 
                 target = getTarget(currentElement)
 
+                # maybe update dictionary about currentElement here?
                 print("target for {} is {}".format(currentElement, target))
+
+                if target == 1:
+                    try:
+                        if d[1] > 0:
+                            newSum.append(1)
+                            # update frequency of 1
+
+                    except:
+                        print("target does not exist in dict")
+
+                elif target > 1:
+                    # try to construct target using keys
+                    constructTarget(target, keys, d)
 
             # define end condition
 
@@ -57,6 +85,8 @@ class Solution:
 
 
         print("Done :)")
+
+        ans += newSum
 
 
 if __name__ == '__main__':
---
+++ 287_find_duplicate_number.py
@@ -0,0 +1,21 @@
+
+
+class Solution:
+    def findDuplicate(self, nums):
+        base = 0
+
+        for i in range(1, len(nums)+1):
+            base += i
+
+        print("base: {} actual: {}".format(base, sum(nums)))
+        print("len(nums)-diff = {}".format(len(nums)-abs(base-sum(nums))))
+
+        return len(nums)-abs(base-sum(nums))
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    # s.findDuplicate([1,3,4,2,2])
+    s.findDuplicate([3,1,3,4,2])
